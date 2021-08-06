9b285dfaa339702c8e6313ccad8dd4d5b1d69126
Gitthuma <george.githuma@gmail.com>
2021-08-07 02:16:23 +0300

    Add try catch statement to catch any errors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ function App() {
   //Create a function called dictionaryApi
   //Create an arrow function
   //Make the arrow funtion async since we are fetchin an Api
+  //Add try catch statement to catch any errors
   const dictionaryApi = async() => {
+
+    try {
+      
+    } catch (error) {
+      
+    }
 
   }
 
