76fa74d2dc4569e9e28659db4309551093098f0f
Gitthuma <george.githuma@gmail.com>
2021-08-07 02:25:01 +0300

    Get the api url using axios

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import './App.css';
 
 function App() {
@@ -7,10 +8,13 @@ function App() {
   //Make the arrow funtion async since we are fetchin an Api
   //Add try catch statement to catch any errors
   //Add a log for the error inside the catch function
+  //Get the api url using axios
   const dictionaryApi = async() => {
 
     try {
-      
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
+        );
     } catch (error) {
       console.log(error);
     }
