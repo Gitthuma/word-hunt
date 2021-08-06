96fd2ed02aeef64abab4e50fefb4b444cfd890c2
Gitthuma <george.githuma@gmail.com>
2021-08-07 02:18:40 +0300

    Add a log for the error inside the catch function

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
   //Create an arrow function
   //Make the arrow funtion async since we are fetchin an Api
   //Add try catch statement to catch any errors
+  //Add a log for the error inside the catch function
   const dictionaryApi = async() => {
 
     try {
       
     } catch (error) {
-      
+      console.log(error);
     }
 
   }
